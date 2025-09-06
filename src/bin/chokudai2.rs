3d20aa5847475f47520d068c45bd16a1c4204af8
imos <173752+imos@users.noreply.github.com>
2025-09-06 09:17:22 +0900

    .

--- src/bin/chokudai2.rs
+++ src/bin/chokudai2.rs
@@ -1,3 +1,12 @@
+#![allow(
+    clippy::collapsible_if,
+    clippy::cast_abs_to_unsigned,
+    clippy::ptr_arg,
+    clippy::needless_return,
+    clippy::len_zero,
+    clippy::needless_range_loop
+)]
+#![allow(unused_imports, unused_variables, unused_mut, unused_parens)]
 use clap::Parser;
 use icfpc2025::judge::*;
 use rand::prelude::*;
