11fa8e5679d6c0afc9f55623ebf019158e0bbc9a
Toshiki Kataoka <tos.lunar@gmail.com>
2025-09-06 10:57:16 +0900

    tos1

---
+++ src/bin/tos1.rs
@@ -0,0 +1,132 @@
+use std::collections::{HashMap, VecDeque};
+
+use icfpc2025::judge::*;
+use rand::prelude::*;
+
+fn fill_doors(graph: &Vec<Vec<usize>>) -> Vec<[(usize, usize); 6]> {
+    let na = !0usize;
+    let mut res = vec![[(na, na); 6]; graph.len()];
+    for (u, edges) in graph.iter().enumerate() {
+        for (door, &v) in edges.iter().enumerate() {
+            res[u][door].0 = v;
+        }
+    }
+    for (u, edges) in graph.iter().enumerate() {
+        for (door, &v) in edges.iter().enumerate() {
+            if res[u][door].1 == na {
+                let v = edges[door];
+                let mut ok = false;
+                for back_door in 0..6 {
+                    if res[v][back_door] == (u, na) {
+                        res[u][door].1 = back_door;
+                        res[v][back_door].1 = door;
+                        ok = true;
+                        break;
+                    }
+                }
+                assert!(ok);
+            }
+        }
+    }
+    res
+}
+
+fn main() {
+    let mut judge = get_judge_from_stdin();
+    let n = judge.num_rooms();
+
+    let mut rng = rand::rng();
+
+    let suffixes = (0..36)
+        .map(|i| {
+            let mut s = vec![i / 6, i % 6];
+            for _ in 0..(16 * n - 2) {
+                s.push(rng.random_range(0..6));
+            }
+            s
+        })
+        .collect::<Vec<_>>();
+
+    let mut queue = VecDeque::new();
+    queue.push_back(vec![]);
+
+    let mut path_to_room: HashMap<Vec<usize>, usize> = HashMap::new();
+    let mut res_to_room: HashMap<Vec<Vec<usize>>, usize> = HashMap::new();
+    let mut room_to_res: Vec<Vec<Vec<usize>>> = vec![];
+    let mut room_to_a_path: Vec<Vec<usize>> = vec![];
+    // let rooms: HashMap<Vec<usize>, _> = HashMap::new();
+
+    let mut cnt = 0;
+    while let Some(path) = queue.pop_front() {
+        // eprintln!("queue len {}", queue.len());
+        cnt += 1;
+        if cnt == 7 * n {
+            return;
+        }
+        let plans = suffixes
+            .iter()
+            .map(|s| {
+                let mut p = path.clone();
+                p.extend(s);
+                p
+            })
+            .collect::<Vec<_>>();
+        let results = judge.explore(&plans);
+        let results = results
+            .into_iter()
+            .map(|r| r[path.len()..].to_vec())
+            .collect::<Vec<_>>();
+        let room = *res_to_room.entry(results.clone()).or_insert_with(|| {
+            let r = room_to_res.len();
+            room_to_res.push(results.clone());
+            room_to_a_path.push(path.clone());
+            for door in 0..6 {
+                let mut p = path.clone();
+                p.push(door);
+                queue.push_back(p);
+            }
+            r
+        });
+        path_to_room.insert(path.clone(), room);
+        eprintln!(
+            "{} {}",
+            path.iter()
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>()
+                .join(""),
+            room
+        );
+    }
+
+    let start = 0;
+    let rooms = room_to_res.iter().map(|r| r[0][0]).collect::<Vec<_>>();
+    let graph: Vec<Vec<usize>> = room_to_a_path
+        .iter()
+        .map(|path| {
+            (0..6)
+                .map(|door| {
+                    let mut p = path.clone();
+                    p.push(door);
+                    *path_to_room.get(&p).unwrap()
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect();
+    let graph = fill_doors(&graph);
+    // let mut graph = vec![];
+    // for (r, path) in room_to_a_path.iter().enumerate() {
+    //     let mut g = [(9999usize, 99usize); 6];
+    //     for door in 0..6 {
+    //         let mut p = path.clone();
+    //         p.push(door);
+    //         let to = *path_to_room.get(&p).unwrap();
+    //         g[door] = (to, 99usize);
+    //     }
+    //     graph.push(g);
+    // }
+    judge.guess(&Guess {
+        start,
+        rooms,
+        graph,
+    });
+}
