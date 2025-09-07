b306dbf79e1b8c8d6422513e33c29e45fcb81483
Toshiki Kataoka <tos.lunar@gmail.com>
2025-09-07 19:21:22 +0900

    normalize (i.e. change) .to.door

--- src/bin/reduce_graph.rs
+++ src/bin/reduce_graph.rs
@@ -11,6 +11,79 @@ use icfpc2025::{
     judge::{Guess, JsonIn},
 };
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+enum Perm3 {
+    // identity
+    I,
+    // swap
+    X,
+    Y,
+    Z,
+    // rotate
+    P,
+    Q,
+}
+
+// impl<F> TryFrom<F> for Perm3
+// where F: AsRef<[usize]>
+impl TryFrom<&Vec<usize>> for Perm3 {
+    type Error = &'static str;
+    fn try_from(v: &Vec<usize>) -> Result<Self, Self::Error> {
+        match v.as_slice() {
+            [0] | [0, 1] | [0, 1, 2] => Ok(Perm3::I),
+            [1, 0] | [1, 0, 2] => Ok(Perm3::X),
+            [2, 1, 0] => Ok(Perm3::Y),
+            [0, 2, 1] => Ok(Perm3::Z),
+            [1, 2, 0] => Ok(Perm3::P),
+            [2, 0, 1] => Ok(Perm3::Q),
+            _ => Err("invalid permutation"),
+        }
+    }
+}
+
+impl std::ops::Neg for Perm3 {
+    type Output = Self;
+    fn neg(self) -> Self::Output {
+        match self {
+            Perm3::P => Perm3::Q,
+            Perm3::Q => Perm3::P,
+            _ => self,
+        }
+    }
+}
+
+fn fill_doors_with_perm(
+    graph: &[Vec<usize>],
+    permutations: &[Vec<Perm3>],
+) -> Vec<[(usize, usize); 6]> {
+    let na = usize::MAX;
+    let mut res = vec![[(na, na); 6]; graph.len()];
+    for (u, edges) in graph.iter().enumerate() {
+        for (door, &v) in edges.iter().enumerate() {
+            res[u][door].0 = v;
+        }
+    }
+    for (u, edges) in graph.iter().enumerate() {
+        for (door, &v) in edges.iter().enumerate() {
+            if res[u][door].1 == na {
+                let mut ok = false;
+                for back_door in 0..6 {
+                    if res[v][back_door] == (u, na)
+                        && permutations[u][door] == -permutations[v][back_door]
+                    {
+                        res[u][door].1 = back_door;
+                        res[v][back_door].1 = door;
+                        ok = true;
+                        break;
+                    }
+                }
+                assert!(ok, "no back door found for {} --{}--> {}", u, door, v);
+            }
+        }
+    }
+    res
+}
+
 fn from_const_vec<T>(v: Vec<T>) -> T
 where
     T: PartialEq + std::fmt::Debug,
@@ -41,11 +114,11 @@ fn main() -> Result<()> {
 
     eprintln!("start = {}", start);
     eprintln!("rooms = {:?}", rooms);
-    let transition = graph
+    let graph = graph
         .iter()
         .map(|v| v.iter().map(|(r, _)| *r).collect::<Vec<_>>())
         .collect::<Vec<_>>();
-    eprintln!("transition = {:?}", transition);
+    eprintln!("graph = {:?}", graph);
 
     let mut eq = vec![vec![true; n]; n];
     for i in 0..n {
@@ -59,8 +132,8 @@ fn main() -> Result<()> {
         for i in 0..n {
             for j in 0..n {
                 for d in 0..6 {
-                    let ni = transition[i][d];
-                    let nj = transition[j][d];
+                    let ni = graph[i][d];
+                    let nj = graph[j][d];
                     new_eq[i][j].setmin(eq[ni][nj]);
                 }
             }
@@ -102,34 +175,43 @@ fn main() -> Result<()> {
     assert_eq!(m * k, n);
     assert!(k <= 3);
 
-    let mut new_graph = vec![vec![(!0, !0); 6]; m];
-    let mut permutations = vec![vec![vec![]; 6]; m];
+    let mut transitions = vec![vec![!0; 6]; m];
+    let mut permutations = vec![vec![Perm3::I; 6]; m];
     for from in 0..m {
         for d in 0..6 {
             let mut to = vec![];
-            let mut back_d = vec![];
             let mut perm = vec![];
             for j in 0..k {
-                let (to_orig, b) = graph[classes[from][j]][d];
+                let from_orig = classes[from][j];
+                let to_orig = graph[from_orig][d];
                 let (t, p) = renamed[to_orig];
                 to.push(t);
-                back_d.push(b);
                 perm.push(p);
             }
             let to = from_const_vec(to);
-            let back_d = from_const_vec(back_d);
-            new_graph[from][d] = (to, back_d);
-            permutations[from][d] = perm;
+            transitions[from][d] = to;
+            permutations[from][d] = (&perm).try_into().unwrap();
         }
     }
 
-    for (i, g) in new_graph.iter().enumerate() {
-        eprint!("{}:", i);
+    {
+        eprint!(" ");
         for d in 0..6 {
-            let (to, back_d) = g[d];
-            eprint!(" {}->{}({})", d, to, back_d);
-            eprintln!();
+            eprint!("    {:2}", d);
         }
+        eprintln!();
+    }
+    for i in 0..m {
+        eprint!("{:2}:", i);
+        for d in 0..6 {
+            eprint!(" {:2}({:?})", transitions[i][d], permutations[i][d]);
+        }
+        eprintln!();
+    }
+
+    let graph = fill_doors_with_perm(&transitions, &permutations);
+    for i in 0..m {
+        eprintln!("{:?}", graph[i]);
     }
     Ok(())
 }
