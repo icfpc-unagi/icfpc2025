e9e5217e67d0701d011cf723dda9b4172afe1cf5
Naohiro Takahashi <chokudai@gmail.com>
2025-09-08 04:23:12 +0900

    D3なのにD=2だったのをなおした

--- src/bin/chokudai_sat_d3.rs
+++ src/bin/chokudai_sat_d3.rs
@@ -22,7 +22,7 @@ fn balanced_plan(len: usize, m: usize, rng: &mut impl Rng) -> Vec<usize> {
 fn main() {
     let mut rng = rand::rng();
     let mut judge = icfpc2025::judge::get_judge_from_stdin();
-    let D = 2; // 倍化率
+    let D = 3; // 倍化率
     let K = 1; // 全体のクエリ数
     let F = judge.num_rooms() * 2; // 前半パートの長さ
     let n = judge.num_rooms() / D;
